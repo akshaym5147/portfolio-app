4351945c3904ffc262ed4469302d735c69b70b46
akshaym5147 <akshaymuge888@gmail.com>
2026-08-18 23:53:42 +0530

    fixed type module related issues

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { fileURLToPath } from 'url'
-import path from 'path'
+import { fileURLToPath } from 'url';
+import path from 'path';
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig(async () => {
   const plugins = [react()];
@@ -26,11 +27,11 @@ export default defineConfig(async () => {
   return {
     plugins,
     base: './',
-      resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+      },
     },
-  },
     build: {
       outDir: 'build',
       sourcemap: true,
@@ -39,11 +40,5 @@ export default defineConfig(async () => {
       host: '0.0.0.0',
       port: 3000,
     },
-    // resolve: {
-    //     alias: {
-    //         '@': path.resolve(__dirname, 'src'),
-    //     },
-    // },
-
   };
 });
